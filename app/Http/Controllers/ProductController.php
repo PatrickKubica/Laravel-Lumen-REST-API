a9fe295c5a4af039e71d0d7c12275a3fc6c0dca3
PatrickKubica <patrickkubica@gmx.de>
2017-12-23 22:06:00 +0100

    fix formatting issues

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,38 +8,37 @@ use Illuminate\Http\Request;
  
 class ProductController extends Controller
 {
+    public function createProduct(Request $request)
+    {
+        $product = Product::create($request->all());
 
-	public function createProduct(Request $request)
-	{
-			$product = Product::create($request->all());
-			
-			return response()->json($product);
-	}
+        return response()->json($product);
+    }
  
-	public function updateProduct(Request $request, $id)
-	{
-      $product = Product::find($id);
-      $product->name = $request->input('name');
-      $product->sku = $request->input('sku');
-      $product->description = $request->input('description');
-      $product->available = $request->input('available');
-      
-      $product->save();
-      return response()->json($product);
-	}  
+    public function updateProduct(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->name = $request->input('name');
+        $product->sku = $request->input('sku');
+        $product->description = $request->input('description');
+        $product->available = $request->input('available');
 
-	public function deleteProduct($id)
-	{  
-      $product = Product::find($id);
-      $product->delete();
+        $product->save();
+        return response()->json($product);
+    }  
 
-      return response()->json('Product was correctly deleted');
-	}
+    public function deleteProduct($id)
+    {  
+        $product = Product::find($id);
+        $product->delete();
 
-	public function index()
-	{
-      $products = Product::all();
- 
-      return response()->json($products);
-	}
+        return response()->json('Product was correctly deleted');
+    }
+
+    public function index()
+    {
+        $products = Product::all();
+
+        return response()->json($products);
+    }
 }
